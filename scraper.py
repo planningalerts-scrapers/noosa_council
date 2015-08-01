583d341690ee8308cd76df9d726511212bee5c5f
Chris Nilsson <christopher@otherchirps.net>
2015-08-01 23:58:54 +1000

    Remove unneeded double-browser create

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import os
 from splinter import Browser
 from morph_planningalerts import DevelopmentApplication, MorphDatabase
 
-from .scraperlib.local_knowledge import (
+from scraperlib.local_knowledge import (
     get_application_links, extract_application_details
 )
 
@@ -10,6 +10,20 @@ from .scraperlib.local_knowledge import (
 DEFAULT_START_URL = "https://noosa-eproperty.t1cloud.com/NOOEPRPROD/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=TM&r=P1.WEBGUEST&f=$P1.ETR.SEARCH.STM"
 
 
+# NOTE: There's a current issue with phantomjs where in some situations,
+#   orphaned processes are left running after the ".quit()" method is called.
+#   See the comments here:
+#     https://github.com/SeleniumHQ/selenium/issues/767, and
+#     https://github.com/detro/ghostdriver/issues/162#issuecomment-105762558
+#
+#   It sounds link, from those comments, that the problem is related to the
+#   npm version of phantom. e.g.:
+#     "It seems removing the npm version of phantom and building it eliminated the issue."
+#
+#   I believe on morph.io, the "built" phantom is in place, so I don't think
+#   the issue is being seen there. But, if suddenly you begin running out of
+#   memory, and have 6GB of zombie phantoms... you're probably hitting this
+#   problem.
 def get_agent(driver_name="phantomjs"):
     return Browser(driver_name)
 
@@ -20,27 +34,28 @@ def main(start_url, custom_agent=None):
 
     count_new = total = 0
 
-    for application_url in get_application_links(agent, start_url):
-        da_info = extract_application_details(agent, application_url)
+    # Run in a with block. Ensures browser.quit()
+    with agent as browser:
+        for application_url in get_application_links(browser, start_url):
+            print(application_url)
+            da_info = extract_application_details(browser, application_url)
 
-        application, created = DevelopmentApplication.create_or_get(
-            **da_info
-        )
+            application, created = DevelopmentApplication.create_or_get(
+                **da_info
+            )
 
-        total += 1
+            total += 1
 
-        if not created:
-            print("Skipping {0.council_reference}".format(application))
-        else:
-            count_new += 1
+            if not created:
+                print("Skipping {0.council_reference}".format(application))
+            else:
+                count_new += 1
 
     print("Added {0} records out of {1} processed.".format(count_new, total))
 
 
 if __name__ == "__main__":
-    with get_agent() as browser:
-        main(
-            start_url=os.environ.get('MORPH_START_URL', DEFAULT_START_URL),
-            agent=browser
-        )
+    main(
+        start_url=os.environ.get('MORPH_START_URL', DEFAULT_START_URL)
+    )
 
